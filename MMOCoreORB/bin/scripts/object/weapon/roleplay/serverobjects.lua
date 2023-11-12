0e03ad270184a4bfaa2dc459974047614c47f9b1
Whiskey <RobinTh11@googlemail.com>
2023-11-12 19:48:21 +0100

    Fixes?

--- MMOCoreORB/bin/scripts/object/weapon/roleplay/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/roleplay/serverobjects.lua
@@ -1,12 +1,5 @@
-includeFile("weapon/roleplay/melee/onehanded/serverobjects.lua")
-includeFile("weapon/roleplay/melee/polearm/serverobjects.lua")
-includeFile("weapon/roleplay/melee/twohanded/serverobjects.lua")
-includeFile("weapon/roleplay/melee/unarmed/serverobjects.lua")
-includeFile("weapon/roleplay/ranged/carbine/serverobjects.lua")
-includeFile("weapon/roleplay/ranged/pistol/serverobjects.lua")
-includeFile("weapon/roleplay/ranged/rifle/serverobjects.lua")
-
 includeFile("weapon/roleplay/thrown/grenade/serverobjects.lua")
+includeFile("weapon/roleplay/ranged/serverobjects.lua")
 
 includeFile("weapon/roleplay/lightsaber/single/1h/serverobjects.lua")
 includeFile("weapon/roleplay/lightsaber/single/2h/serverobjects.lua")
