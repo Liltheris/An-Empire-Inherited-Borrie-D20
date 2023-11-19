4544fc42fe746525c2b652ea3e2bed9142b6e229
Whiskey <RobinTh11@googlemail.com>
2023-11-19 12:56:21 +0100

    Fixed Zabrak hairstyles not having server definitions.

---
+++ MMOCoreORB/bin/scripts/object/tangible/hair/zabrak/hair_zabrak_female_s22.lua
@@ -0,0 +1,5 @@
+object_tangible_hair_zabrak_hair_zabrak_female_s22 = object_tangible_hair_zabrak_shared_hair_zabrak_female_s22:new {
+
+}
+
+ObjectTemplates:addTemplate(object_tangible_hair_zabrak_hair_zabrak_female_s22, "object/tangible/hair/zabrak/hair_zabrak_female_s22.iff")
---
+++ MMOCoreORB/bin/scripts/object/tangible/hair/zabrak/hair_zabrak_female_s41.lua
@@ -0,0 +1,5 @@
+object_tangible_hair_zabrak_hair_zabrak_female_s41 = object_tangible_hair_zabrak_shared_hair_zabrak_female_s41:new {
+
+}
+
+ObjectTemplates:addTemplate(object_tangible_hair_zabrak_hair_zabrak_female_s41, "object/tangible/hair/zabrak/hair_zabrak_female_s41.iff")
