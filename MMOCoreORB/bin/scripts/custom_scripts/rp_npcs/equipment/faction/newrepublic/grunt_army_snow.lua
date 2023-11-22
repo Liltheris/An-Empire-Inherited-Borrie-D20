82699f2f2e6b6d36ae0e5989e1af3a31789a2947
Hannah <Liltheris@gmail.com>
2023-11-22 13:47:21 +0000

    custom_scripts backup

---
+++ MMOCoreORB/bin/scripts/custom_scripts/rp_npcs/equipment/faction/newrepublic/grunt_army_snow.lua
@@ -0,0 +1,10 @@
+equipment = {
+	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+	{"object/tangible/wearables/gloves/gloves_s02.iff", "/private/index_color_1", 31, },
+	{"object/tangible/wearables/shirt/shirt_s09.iff", "/private/index_color_2", 185, "/private/index_color_1", 185, },
+	{"object/tangible/wearables/backpack/backpack_rebel_snow_soldier.iff", },
+	{"object/tangible/wearables/boots/boots_s34.iff", },
+	{"object/tangible/wearables/jacket/jacket_s06.iff", "/private/index_color_2", 42, "/private/index_color_1", 2, },
+	{"object/tangible/wearables/armor/roleplay/rebel_snow/armor_rebel_snow_helmet.iff", },
+	{"object/tangible/wearables/pants/pants_s21.iff", "/private/index_color_2", 42, "/private/index_color_1", 42, },
+}
