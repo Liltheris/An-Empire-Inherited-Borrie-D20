4d9a260003f87d74984f06b4f9a3d6fbabfe8df4
Whiskey <RobinTh11@googlemail.com>
2023-11-27 11:33:34 +0100

    New imperial kits

---
+++ MMOCoreORB/bin/custom_scripts/rp_objects/container/faction/empire/loadout/grunt_army.lua
@@ -0,0 +1,8 @@
+containerManifest = {
+	{"object/tangible/wearables/hat/shared_hat_imp_veers_s01.iff", },
+	{"object/tangible/wearables/boots/shared_boots_s14.iff", },
+	{"object/tangible/wearables/pants/shared_pants_s15.iff", },
+	{"object/tangible/wearables/gloves/shared_gloves_s02.iff", "/private/index_color_1", 31, },
+	{"object/tangible/wearables/jacket/shared_jacket_imp_veers.iff", },
+	{"object/weapon/roleplay/ranged/carbine/dc15s_carbine.iff",},
+}
