40aefa0f598d199be52f73cf176537923166a58f
Whiskey <RobinTh11@googlemail.com>
2023-12-06 12:03:02 +0100

    Added news headlines

---
+++ MMOCoreORB/bin/scripts/screenplays/borrie/holonet/headlines/headlines_dantooine.lua
@@ -0,0 +1,7 @@
+headline = {
+	planet = "rp_dantooine",
+	title = "Unusual Activity near Damarise Outpost",
+	content = "Farmers and hikers alike are reporting an unusual increase in Pentastar Patrol activity near the small settlement of Damarise Outpost. While military officials have not published an official response yet, prevailing theories indicate that an increase in pirate activity are finally being addressed by the security forces.",
+	galaxyDate = "06.12.6 AE",
+}
+BorNewsnet:addHeadline(headline)
