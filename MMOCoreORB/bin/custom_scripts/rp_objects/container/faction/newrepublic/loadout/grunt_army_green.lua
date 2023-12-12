ceacc3bda6cc3f222816531ea40961c35c24fcb2
Whiskey <RobinTh11@googlemail.com>
2023-12-12 18:11:16 +0100

    New Republic equipment loadouts

---
+++ MMOCoreORB/bin/custom_scripts/rp_objects/container/faction/newrepublic/loadout/grunt_army_green.lua
@@ -0,0 +1,13 @@
+containerManifest = {
+	{"object/tangible/wearables/boots/shared_boots_s05.iff", "/private/index_color_2", 31, "/private/index_color_1", 37, },
+	{"object/tangible/wearables/gloves/shared_gloves_s02.iff", "/private/index_color_1", 31, },
+	{"object/tangible/wearables/helmet/shared_helmet_s06.iff", },
+	{"object/tangible/wearables/shirt/shared_shirt_s09.iff", "/private/index_color_2", 194, "/private/index_color_1", 194, },
+	{"object/tangible/wearables/jacket/shared_jacket_s24.iff", "/private/index_color_1", 69, },
+	{"object/tangible/wearables/belt/shared_npe_belt_01.iff", "/private/index_color_1", 30, },
+	{"object/tangible/wearables/backpack/shared_empireday_rebel_endor_backpack.iff", },
+	{"object/tangible/wearables/pants/shared_pants_s07.iff", "/private/index_color_2", 130, "/private/index_color_1", 26, },
+	
+	{"object/weapon/roleplay/ranged/rifle/a280_blaster_rifle.iff",},
+	{"object/weapon/roleplay/ranged/pistol/scout_blaster_pistol.iff",},
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_objects/container/faction/newrepublic/loadout/scout.lua
@@ -0,0 +1,11 @@
+containerManifest = {
+	{"object/tangible/wearables/jacket/shared_jacket_s21.iff", },
+	{"object/tangible/wearables/pants/shared_pants_s07.iff", "/private/index_color_2", 28, "/private/index_color_1", 34, },
+	{"object/tangible/wearables/boots/shared_boots_s05.iff", "/private/index_color_2", 31, "/private/index_color_1", 37, },
+	{"object/tangible/wearables/gloves/shared_gloves_s02.iff", "/private/index_color_1", 31, },
+	{"object/tangible/wearables/shirt/shared_shirt_s10.iff", "/private/index_color_1", 16, },
+	{"object/tangible/wearables/bandolier/shared_bandolier_s04.iff", },
+	
+	{"object/weapon/roleplay/ranged/rifle/dlt20a_heavy_blaster_rifle.iff",},
+	{"object/weapon/roleplay/ranged/pistol/dl44_blaster_pistol.iff",},
+}
