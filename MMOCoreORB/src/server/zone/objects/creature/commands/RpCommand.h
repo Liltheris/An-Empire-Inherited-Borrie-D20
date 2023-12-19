bb5e3c959869f780c501e6e1e2dc62186440b17e
Whiskey <RobinTh11@googlemail.com>
2023-12-19 21:19:30 +0100

    Reload commands

--- MMOCoreORB/src/server/zone/objects/creature/commands/RpCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RpCommand.h
@@ -94,13 +94,17 @@ public:
 					}
 				}
 			} else if(command == "weapon") {
-				if(creature->getWeapon() != nullptr) {
-					int wepDmg = creature->getWeapon()->getMaxCondition() - creature->getWeapon()->getConditionDamage();
-					creature->sendSystemMessage("Current Weapon Condition: " + String::valueOf(wepDmg) + "/" + creature->getWeapon()->getMaxCondition());
+				ManagedReference<WeaponObject*> weapon = creature->getWeapon();
+				if(weapon != nullptr) {
+					int wepDmg = weapon->getMaxCondition() - weapon->getConditionDamage();
+					creature->sendSystemMessage("Current Weapon Condition: " + String::valueOf(wepDmg) + "/" + weapon->getMaxCondition());
+					if(weapon->getAmmoPack() != ""){
+						creature->sendSystemMessage("Weapon Ammo: " +String::valueOf(weapon->getMaxAmmo() - weapon->getStoredInt("ammo_used")) + "/" + weapon->getMaxAmmo());
+					}
 					if(creature->getWeapon()->isJediWeapon()) {
 						creature->sendSystemMessage("Your weapon is considered a Force User's weapon.");
 					}
-				}				
+				}
 			} else if (command == "target") {
 				BorrieRPG::SaveTarget(creature, object);
 			} else if(command == "meditate") {
