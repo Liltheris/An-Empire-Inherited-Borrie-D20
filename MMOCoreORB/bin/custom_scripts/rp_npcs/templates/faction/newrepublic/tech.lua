b3504e511da09b45704d9ec92e0fdd74066c012e
Whiskey <RobinTh11@googlemail.com>
2024-01-21 15:13:09 +0100

    fixed templates lacking equipment

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/newrepublic/tech.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/newrepublic/tech.lua
@@ -16,11 +16,31 @@ npc_template = {
 	equipmentTemplates = {
 		{"rp_human_male", {"faction/newrepublic/tech"}},
 		{"rp_human_female", {"faction/newrepublic/tech"}},
+		{"rp_sullustan_male", {"faction/newrepublic/tech"}},
+		{"rp_sullustan_female", {"faction/newrepublic/tech"}},
+		{"rp_zabrak_male", {"faction/newrepublic/tech"}},
+		{"rp_zabrak_female", {"faction/newrepublic/tech"}},
+		{"rp_mirialan_male", {"faction/newrepublic/tech"}},
+		{"rp_mirialan_female", {"faction/newrepublic/tech"}},
+		{"rp_moncal_male", {"faction/newrepublic/tech"}},
+		{"rp_moncal_female", {"faction/newrepublic/tech"}},
+		{"rp_twilek_male", {"faction/newrepublic/tech"}},
+		{"rp_twilek_female", {"faction/newrepublic/tech"}},
 	},	
 	
 	customizationTemplates = {
 		{"rp_human_male", {"random"}},
 		{"rp_human_female", {"random"}},
+		{"rp_sullustan_male", {"random"}},
+		{"rp_sullustan_female", {"random"}},
+		{"rp_zabrak_male", {"random"}},
+		{"rp_zabrak_female", {"random"}},
+		{"rp_mirialan_male", {"random"}},
+		{"rp_mirialan_female", {"random"}},
+		{"rp_moncal_male", {"random"}},
+		{"rp_moncal_female", {"random"}},
+		{"rp_twilek_male", {"random"}},
+		{"rp_twilek_female", {"random"}},
 	},	
 	
 	customVarOverrides = {
