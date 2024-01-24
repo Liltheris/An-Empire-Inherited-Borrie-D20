4b1acf24b2cfc2a81123c52dd9e1851e5def415d
Whiskey <RobinTh11@googlemail.com>
2024-01-24 13:52:29 +0100

    New items server files (+maybe fix imp army chest plate)

--- MMOCoreORB/bin/scripts/object/tangible/wearables/cape/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/cape/objects.lua
@@ -25,4 +25,9 @@ object_tangible_wearables_cape_shared_cape_short = SharedTangibleObjectTemplate:
 }
 ObjectTemplates:addClientTemplate(object_tangible_wearables_cape_shared_cape_short, "object/tangible/wearables/cape/shared_cape_short.iff")
 --********************************************************************************************************************************************
+object_tangible_wearables_cape_shared_poncho_s01 = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/cape/shared_poncho_s01.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_wearables_cape_shared_poncho_s01, "object/tangible/wearables/cape/shared_poncho_s01.iff")
+--**************************************************************************************************************************************************
 
---
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/cape/poncho_s01.lua
@@ -0,0 +1,4 @@
+object_tangible_wearables_cape_poncho_s01 = object_tangible_wearables_cape_shared_poncho_s01:new {
+
+}
+ObjectTemplates:addTemplate(object_tangible_wearables_cape_poncho_s01, "object/tangible/wearables/cape/poncho_s01.iff")
