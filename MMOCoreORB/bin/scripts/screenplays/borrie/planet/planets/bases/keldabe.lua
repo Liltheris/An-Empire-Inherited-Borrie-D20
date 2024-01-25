bfed4a1b5f1feb46974124363fdb6c4fb9189c69
Whiskey <RobinTh11@googlemail.com>
2024-01-25 13:26:33 +0100

    First pass of Keldabe NPCs

---
+++ MMOCoreORB/bin/scripts/screenplays/borrie/planet/planets/bases/keldabe.lua
@@ -0,0 +1,31 @@
+base = {
+	tag = "keldabe",
+	name = "Keldabe City",
+	defaultOwner = "mando",
+	
+	flags = {
+	
+	},
+	
+	spawns = {
+		--Type, Zone, x, z, y, angle, cell
+		--Capitol Area
+		{"trooper","rp_mandalore", -3723.5, 35, -2919.16, 0, 0,},
+		{"trooper","rp_mandalore", -3737.29, 35, -2919.35, 0, 0,},
+		{"trooper","rp_mandalore", -3722.33, 36.2812, -2979.79, 0, 0,},
+		{"trooper","rp_mandalore", -3738.42, 36.2822, -2979.91, 0, 0,},
+		{"trooper","rp_mandalore", -3735.6, 35.1963, -3004.36, 0, 0,},
+		{"trooper","rp_mandalore", -3732.35, 35.1963, -3004.34, 0, 0,},
+		{"trooper","rp_mandalore", -3728.41, 35.1963, -3004.34, 0, 0,},
+		{"trooper","rp_mandalore", -3725.21, 35.1973, -3004.36, 0, 0,},
+		--Spaceport
+		{"trooper","rp_mandalore", -3805.76, 35, -2611.77, 136, 0,},
+		{"trooper","rp_mandalore", -3798.48, 35, -2604.24, 137, 0,},
+		{"trooper","rp_mandalore", -3744.36, 35, -2573.4, 183, 0,},
+		{"trooper","rp_mandalore", -3734.23, 35, -2573.47, 184, 0,},
+		{"trooper","rp_mandalore", -3809.18, 36, -2639.29, 48, 0,},
+		{"trooper","rp_mandalore", -3772.69, 35, -2642.55, 134, 0,},
+	}
+}
+
+BorPlanetManager:addBase(base)
