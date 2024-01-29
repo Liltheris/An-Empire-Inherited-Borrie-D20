f2d6491f37a57c6f99a54768dd97a26920acfa11
Whiskey <RobinTh11@googlemail.com>
2024-01-29 10:19:01 +0100

    faction pre-work

--- MMOCoreORB/bin/scripts/screenplays/borrie/planet/planets/rp_mandalore.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/planet/planets/rp_mandalore.lua
@@ -9,8 +9,8 @@ planet = {
 	
 	landing_points = {
 		--Tag, Name, Zone, x, z, y, angle, cell, shipVisible
-		{"keldabe_starport", "Keldabe", "rp_mandalore", -3860.0, 0.0, -2550.0, 0, true,},
-	}	
+		{"keldabe_starport", "Keldabe", "rp_mandalore", -3860.0, 0.0, -2550.0, 0, 0, true},
+	}
 }
 
 BorPlanetManager:addPlanet(planet)
