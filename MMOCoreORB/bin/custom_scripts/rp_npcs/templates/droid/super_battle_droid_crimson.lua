49a5231d7d670555370e5f238c17fa2b9490146a
Whiskey <RobinTh11@googlemail.com>
2024-02-18 20:34:03 +0100

    Create super_battle_droid_crimson.lua

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/droid/super_battle_droid_crimson.lua
@@ -0,0 +1,12 @@
+npc_template = {
+
+	name = "B2 Battle Droid",
+	baseTemplates = {"super_battle_droid", 1},
+	skillTemplates = {"factional/common/elite"},
+	
+	customVarOverrides = {
+		{"/private/index_texture_1", 9},
+		{"/private/index_color_1", 23},
+		{"/private/index_color_2", 15},
+	}
+}
