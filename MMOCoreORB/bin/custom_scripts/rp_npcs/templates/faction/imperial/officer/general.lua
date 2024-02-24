b731fbc55a88c21291cf698b7a5565a51512d98a
Whiskey <RobinTh11@googlemail.com>
2024-02-24 19:29:57 +0100

    Officer templates

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/imperial/officer/general.lua
@@ -0,0 +1,19 @@
+npc_template = {
+
+	randomName = "human",
+	baseTemplates = {
+		"rp_human_male", 85,
+		"rp_human_female", 15
+	},
+	skillTemplates = {"factional/common/officer"},
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/imperial/officer_general"}},
+		{"rp_human_female", {"faction/imperial/officer_general"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random_military"}},
+		{"rp_human_female", {"random_military"}},
+	},	
+}
