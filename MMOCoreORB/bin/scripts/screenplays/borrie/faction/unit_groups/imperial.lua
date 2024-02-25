55fa9f988102f1afa7c38309b5d67d86f4c07d7c
Whiskey <RobinTh11@googlemail.com>
2024-02-25 10:23:50 +0100

    Update imperial.lua

--- MMOCoreORB/bin/scripts/screenplays/borrie/faction/unit_groups/imperial.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/faction/unit_groups/imperial.lua
@@ -1,6 +1,7 @@
 unitGroup = {
 	tag = "imperial",
 	units = {
+		--Original
 		["recruiter"] = "faction/imperial/recruiter",
 		["grunt_army"] = "faction/imperial/grunt_army",
 		["grunt_navy"] = "faction/imperial/grunt_navy",
@@ -17,6 +18,25 @@ unitGroup = {
 		["tech"] = "faction/imperial/tech",
 		["tech_lead"] = "faction/imperial/tech_lead",
 		["tech_medic"] = "faction/imperial/tech_medic",
+		--Officer
+		["officer/admiral"] = "faction/imperial/officer/admiral",
+		["officer/captain"] = "faction/imperial/officer/captain",
+		["officer/colonel"] = "faction/imperial/officer/colonel",
+		["officer/commander"] = "faction/imperial/officer/commander",
+		["officer/general"] = "faction/imperial/officer/general",
+		["officer/lieutenant"] = "faction/imperial/officer/lieutenant",
+		["officer/major"] = "faction/imperial/officer/major",
+		["officer/moff"] = "faction/imperial/officer/moff",
+		
+		--Navy
+		["navy/gunner"] = "faction/imperial/navy/gunner",
+		["navy/pilot"] = "faction/imperial/navy/pilot",
+		["navy/tech"] = "faction/imperial/navy/tech",
+		["navy/trooper"] = "faction/imperial/navy/trooper",
+		--Army
+		["army/medic"] = "faction/imperial/army/medic",
+		["army/pilot"] = "faction/imperial/army/pilot",
+		["army/trooper"] = "faction/imperial/army/trooper",
 	}
 }
 
