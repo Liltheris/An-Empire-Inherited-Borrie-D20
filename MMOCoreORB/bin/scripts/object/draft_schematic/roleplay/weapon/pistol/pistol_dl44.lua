9017c3d74bf6f273781eb20ec75506ba9b5f8e96
Hannah <Liltheris@gmail.com>
2024-03-07 18:00:00 +0000

    Pistol crafting server files

--- MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/pistol_dl44.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/pistol_dl44.lua
@@ -2,10 +2,10 @@ object_draft_schematic_roleplay_weapon_pistol_pistol_d18 = object_draft_schemati
 
 	templateType = DRAFTSCHEMATIC,
 
-	customObjectName = "D-18 Blaster Pistol",
+	customObjectName = "DL-44 Blaster Pistol",
 
 	craftingToolTab = 1, -- (See DraftSchematicObjectTemplate.h)
-	complexity = 8,
+	complexity = 12,
 	size = 1,
 	factoryCrateType = "object/factory/factory_crate_weapon.iff",
 
@@ -24,12 +24,12 @@ object_draft_schematic_roleplay_weapon_pistol_pistol_d18 = object_draft_schemati
 	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
 	ingredientTitleNames = {"receiver","operating_system","barrel","scope"},
 	ingredientSlotType = {1,1,3,3},
-	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_pistol_common.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff","object/tangible/component/weapon/shared_blaster_pistol_barrel.iff","object/tangible/component/weapon/shared_scope_weapon.iff"},
+	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_pistol_uncommon.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff","object/tangible/component/weapon/shared_blaster_pistol_barrel.iff","object/tangible/component/weapon/shared_scope_weapon.iff"},
 	resourceQuantities = {1,1,1,1},
 	contribution = {100,100,100,100},
 	ingredientAppearance = {"", "", "muzzle", "scope"},
-	targetTemplate = "object/weapon/roleplay/ranged/pistol/d18_pistol.iff",
+	targetTemplate = "object/weapon/roleplay/ranged/pistol/pistol_dl44.iff",
 
 	additionalTemplates = {}
 
-}ObjectTemplates:addTemplate(object_draft_schematic_roleplay_weapon_pistol_pistol_d18, "object/draft_schematic/roleplay/weapon/pistol/pistol_d18.iff")
+}ObjectTemplates:addTemplate(object_draft_schematic_roleplay_weapon_pistol_pistol_d18, "object/draft_schematic/roleplay/weapon/pistol/pistol_dl44.iff")
---
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/pistol_striker.lua
@@ -0,0 +1,35 @@
+object_draft_schematic_roleplay_weapon_pistol_pistol_d18 = object_draft_schematic_roleplay_weapon_pistol_shared_pistol_d18:new {
+
+	templateType = DRAFTSCHEMATIC,
+
+	customObjectName = "Striker Blaster Pistol",
+
+	craftingToolTab = 1, -- (See DraftSchematicObjectTemplate.h)
+	complexity = 18,
+	size = 1,
+	factoryCrateType = "object/factory/factory_crate_weapon.iff",
+
+	xpType = "",
+	xp = 0,
+
+	assemblySkill = "",
+	experimentingSkill = "",
+	customizationSkill = "",
+ 	factoryCrateSize = 0,
+
+	customizationOptions = {},
+	customizationStringNames = {},
+	customizationDefaults = {},
+
+	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
+	ingredientTitleNames = {"receiver","operating_system"},
+	ingredientSlotType = {1,1},
+	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_pistol_common.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff"},
+	resourceQuantities = {1,1},
+	contribution = {100,100},
+	ingredientAppearance = {"", ""},
+	targetTemplate = "object/weapon/roleplay/ranged/pistol/striker_pistol.iff",
+
+	additionalTemplates = {}
+
+}ObjectTemplates:addTemplate(object_draft_schematic_roleplay_weapon_pistol_pistol_d18, "object/draft_schematic/roleplay/weapon/pistol/pistol_striker.iff")
