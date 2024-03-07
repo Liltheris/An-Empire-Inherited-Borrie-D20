95aa02ae7d719529193370fcc9f2c08e3f861251
Hannah <Liltheris@gmail.com>
2024-03-07 18:05:55 +0000

    Update objects.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/objects.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/objects.lua
@@ -3,3 +3,44 @@ object_draft_schematic_roleplay_weapon_pistol_shared_pistol_d18 = SharedDraftSch
 }
 ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_d18, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_d18.iff")
 --********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_cdef = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_cdef.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_cdef, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_cdef.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_dd6 = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_dd6.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_dd6, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_dd6.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_dl44_metal = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_dl44_metal.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_dl44_metal, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_dl44_metal.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_dl44 = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_dl44.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_dl44, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_dl44.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_fwg5 = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_fwg5.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_fwg5, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_fwg5.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_striker = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_striker.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_striker, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_striker.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_westar_34 = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_westar_34.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_westar_34, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_westar_34.iff")
+--********************************************************************************************************************************
+object_draft_schematic_roleplay_weapon_pistol_shared_pistol_westar_35 = SharedDraftSchematicObjectTemplate:new {
+	clientTemplateFileName = "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_westar_35.iff"
+}
+ObjectTemplates:addClientTemplate(object_draft_schematic_roleplay_weapon_pistol_shared_pistol_westar_35, "object/draft_schematic/roleplay/weapon/pistol/shared_pistol_westar_35.iff")
+--********************************************************************************************************************************
+
