059d3199999861ea6db2e1361041b220335fda21
Hannah <Liltheris@gmail.com>
2024-03-08 18:39:18 +0000

    Update schematics.lua

--- MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
+++ MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
@@ -205,11 +205,11 @@ schematics = {
 	{path="object/draft_schematic/roleplay/component/comp_op_system_kinetic.iff"},
 	{path="object/draft_schematic/roleplay/component/comp_op_system_stun.iff"},
 
-	-- {path="object/draft_schematic/roleplay/component/comp_weapon_parts_common.iff"},
-	-- {path="object/draft_schematic/roleplay/component/comp_weapon_parts_uncommon.iff"},
-	-- {path="object/draft_schematic/roleplay/component/comp_weapon_parts_rare.iff"},
-	-- {path="object/draft_schematic/roleplay/component/comp_weapon_parts_epic.iff"},
-	-- {path="object/draft_schematic/roleplay/component/comp_weapon_parts_legenadry.iff"},
+	{path="object/draft_schematic/roleplay/component/comp_weapon_parts_common.iff"},
+	{path="object/draft_schematic/roleplay/component/comp_weapon_parts_uncommon.iff"},
+	{path="object/draft_schematic/roleplay/component/comp_weapon_parts_rare.iff"},
+	{path="object/draft_schematic/roleplay/component/comp_weapon_parts_epic.iff"},
+	{path="object/draft_schematic/roleplay/component/comp_weapon_parts_legenadry.iff"},
 
 	-- {path="object/draft_schematic/roleplay/component/comp_weapon_receiver_carbine_common.iff"},
 	-- {path="object/draft_schematic/roleplay/component/comp_weapon_receiver_carbine_uncommon.iff"},
