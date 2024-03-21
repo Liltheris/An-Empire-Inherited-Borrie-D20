27f9f3d4929db8d5c72306003b03406a2f4eb8e3
Hannah <Liltheris@gmail.com>
2024-03-21 18:05:53 +0000

    mask + start of speeder crafting serv files x

--- MMOCoreORB/bin/scripts/object/tangible/wearables/mask/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/mask/objects.lua
@@ -3,3 +3,8 @@ object_tangible_wearables_mask_shared_mask_sith_s01 = SharedTangibleObjectTempla
 }
 ObjectTemplates:addClientTemplate(object_tangible_wearables_mask_shared_mask_sith_s01, "object/tangible/wearables/mask/shared_mask_sith_s01.iff")
 --**************************************************************************************************************************************************
+object_tangible_wearables_mask_shared_mask_bandana_s01 = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/mask/shared_mask_bandana_s01.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_wearables_mask_shared_bandana_s01, "object/tangible/wearables/mask/shared_mask_bandana_s01.iff")
+--**************************************************************************************************************************************************
