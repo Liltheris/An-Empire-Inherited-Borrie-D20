05a5335ce1bb1a5ed6f2ebfb90eb4596dfb9d4a0
Whiskey <RobinTh11@googlemail.com>
2024-03-22 20:01:14 +0100

    Refactored skill data storage.

--- MMOCoreORB/src/server/zone/borrie/BorSkill.h
+++ MMOCoreORB/src/server/zone/borrie/BorSkill.h
@@ -7,287 +7,39 @@
 #include "engine/util/u3d/Coordinate.h"
 #include "server/zone/managers/skill/SkillManager.h"
 
-//#include "templates/roleplay/RoleplayManager.h"
+#include "server/zone/managers/roleplay/RoleplayManager.h"
 
 #include "server/db/ServerDatabase.h"
 
 class BorSkill : public Logger {
 public:
 	static bool GetStringIsAttribute(String input) {
-		if (input == "strength")
-			return true;
-		else if (input == "precision")
-			return true;
-		else if (input == "dexterity")
-			return true;
-		else if (input == "awareness")
-			return true;
-		else if (input == "intelligence")
-			return true;
-		else if (input == "charisma")
-			return true;
-		else if (input == "constitution")
-			return true;
-		else if (input == "mindfulness")
-			return true;
-		else
-			return false;
+		RoleplayManager* rp = RoleplayManager::instance();
+		return (rp->getRpSkillIndex(input, RpSkillType::ATTRIBUTE) != -1);
 	}
 
 	static String GetSkillParent(String skill) {
-		if (skill == "melee")
-			return "strength";
-		else if (skill == "intimidation")
-			return "strength";
-		else if (skill == "unarmed")
-			return "strength";
-		else if (skill == "lightsaber")
-			return "strength";
-		else if (skill == "ranged")
-			return "precision";
-		else if (skill == "mechanics")
-			return "precision";
-		else if (skill == "demolitions")
-			return "precision";
-		else if (skill == "engineering")
-			return "precision";
-		else if (skill == "larceny")
-			return "dexterity";
-		else if (skill == "stealth")
-			return "dexterity";
-		else if (skill == "maneuverability")
-			return "dexterity";
-		else if (skill == "throwing")
-			return "dexterity";
-		else if (skill == "investigation")
-			return "awareness";
-		else if (skill == "piloting")
-			return "awareness";
-		else if (skill == "survival")
-			return "awareness";
-		else if (skill == "sense")
-			return "awareness";
-		else if (skill == "slicing")
-			return "intelligence";
-		else if (skill == "computers")
-			return "intelligence";
-		else if (skill == "medicine")
-			return "intelligence";
-		else if (skill == "science")
-			return "intelligence";
-		else if (skill == "persuasion")
-			return "charisma";
-		else if (skill == "bluff")
-			return "charisma";
-		else if (skill == "composure")
-			return "charisma";
-		else if (skill == "resolve")
-			return "charisma";
-		else if (skill == "athletics")
-			return "constitution";
-		else if (skill == "lightning")
-			return "constitution";
-		else if (skill == "armor")
-			return "constitution";
-		else if (skill == "defending")
-			return "constitution";
-		else if (skill == "telekinesis")
-			return "mindfulness";
-		else if (skill == "control")
-			return "mindfulness";
-		else if (skill == "alter")
-			return "mindfulness";
-		else if (skill == "inward")
-			return "mindfulness";
-		else if (skill == "performance")
-			return "charisma";
-		else if (skill == "command")
-			return "charisma";
-		else
-			return "";
+		RoleplayManager* rp = RoleplayManager::instance();
+		return rp->getRpSkill(rp->getRpSkillIndex(skill), RpSkillType::SKILL).parentSkillName;
 	}
 
 	static String GetSkillAltParent(String skill) {
-		if (skill == "melee")
-			return "dexterity";
-		else if (skill == "intimidation")
-			return "constitution";
-		else if (skill == "unarmed")
-			return "dexterity";
-		else if (skill == "lightsaber")
-			return "dexterity";
-		else if (skill == "ranged")
-			return "dexterity";
-		else if (skill == "mechanics")
-			return "intelligence";
-		else if (skill == "demolitions")
-			return "awareness";
-		else if (skill == "engineering")
-			return "intelligence";
-		else if (skill == "larceny")
-			return "charisma";
-		else if (skill == "stealth")
-			return "awareness";
-		else if (skill == "maneuverability")
-			return "awareness";
-		else if (skill == "throwing")
-			return "strength";
-		else if (skill == "investigation")
-			return "intelligence";
-		else if (skill == "piloting")
-			return "intelligence";
-		else if (skill == "survival")
-			return "constitution";
-		else if (skill == "sense")
-			return "mindfulness";
-		else if (skill == "slicing")
-			return "precision";
-		else if (skill == "computers")
-			return "precision";
-		else if (skill == "medicine")
-			return "precision";
-		else if (skill == "science")
-			return "mindfulness";
-		else if (skill == "persuasion")
-			return "awareness";
-		else if (skill == "bluff")
-			return "intelligence";
-		else if (skill == "composure")
-			return "mindfulness";
-		else if (skill == "resolve")
-			return "constitution";
-		else if (skill == "athletics")
-			return "dexterity";
-		else if (skill == "lightning")
-			return "intelligence";
-		else if (skill == "armor")
-			return "strength";
-		else if (skill == "defending")
-			return "strength";
-		else if (skill == "telekinesis")
-			return "precision";
-		else if (skill == "control")
-			return "charisma";
-		else if (skill == "alter")
-			return "strength";
-		else if (skill == "inward")
-			return "intelligence";
-		else if (skill == "performance")
-			return "mindfulness";
-		else if (skill == "command")
-			return "intelligence";
-		else
-			return "";
+		RoleplayManager* rp = RoleplayManager::instance();
+		return rp->getRpSkill(rp->getRpSkillIndex(skill), RpSkillType::SKILL).altParentSkill;
 	}
 
 	static bool GetStringIsSkill(String skill) {
-		if (skill == "melee")
-			return true;
-		else if (skill == "intimidation")
-			return true;
-		else if (skill == "unarmed")
-			return true;
-		else if (skill == "lightsaber")
-			return true;
-		else if (skill == "ranged")
-			return true;
-		else if (skill == "mechanics")
-			return true;
-		else if (skill == "demolitions")
-			return true;
-		else if (skill == "engineering")
-			return true;
-		else if (skill == "larceny")
-			return true;
-		else if (skill == "stealth")
-			return true;
-		else if (skill == "maneuverability")
-			return true;
-		else if (skill == "throwing")
-			return true;
-		else if (skill == "investigation")
-			return true;
-		else if (skill == "piloting")
-			return true;
-		else if (skill == "survival")
-			return true;
-		else if (skill == "sense")
-			return true;
-		else if (skill == "slicing")
-			return true;
-		else if (skill == "computers")
-			return true;
-		else if (skill == "medicine")
-			return true;
-		else if (skill == "science")
-			return true;
-		else if (skill == "persuasion")
-			return true;
-		else if (skill == "bluff")
-			return true;
-		else if (skill == "composure")
-			return true;
-		else if (skill == "resolve")
-			return true;
-		else if (skill == "athletics")
-			return true;
-		else if (skill == "lightning")
-			return true;
-		else if (skill == "armor")
-			return true;
-		else if (skill == "defending")
-			return true;
-		else if (skill == "telekinesis")
-			return true;
-		else if (skill == "control")
-			return true;
-		else if (skill == "alter")
-			return true;
-		else if (skill == "inward")
-			return true;
-		else if (skill == "performance")
-			return true;
-		else if (skill == "command")
-			return true;
-		else
-			return false;
+		RoleplayManager* rp = RoleplayManager::instance();
+		return (rp->getRpSkillIndex(skill, RpSkillType::SKILL) != -1);
 	}
 
 	static bool GetStringIsForceSkill(String skill) {
-		if (skill == "telekinesis")
-			return true;
-		else if (skill == "control")
-			return true;
-		else if (skill == "alter")
-			return true;
-		else if (skill == "inward")
-			return true;
-		else if (skill == "lightning")
-			return true;
-		else if (skill == "lightsaber")
-			return true;
-		else if (skill == "sense")
-			return true;
-		else 
-			return false;
+		RoleplayManager* rp = RoleplayManager::instance();
+		return (rp->getRpSkillIndex(skill, RpSkillType::FORCESKILL) != -1);
 	}
 
 	static bool GetSkillIsForceSkill(String skill) {
-		if(skill.contains("telekinesis"))
-			return true;
-		else if(skill.contains("control"))
-			return true;
-		else if(skill.contains("alter"))
-			return true;
-		else if(skill.contains("inward"))
-			return true;
-		else if(skill.contains("lightning"))
-			return true;
-		else if(skill.contains("lightsaber"))
-			return true;
-		else if(skill.contains("sense"))
-			return true;
-		else return false;
+		return GetStringIsForceSkill(GetSkillRealName(skill));
 	}
 
 	static String GetSkillRealName(String input) {
