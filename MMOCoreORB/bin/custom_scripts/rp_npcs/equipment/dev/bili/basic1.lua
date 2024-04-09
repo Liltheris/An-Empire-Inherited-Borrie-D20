3fba7710ccc56a5aebc9d480962747af33f4ff4f
Valerie <liltheris@gmail.com>
2024-04-09 18:56:54 +0100

    pushback

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/dev/bili/basic1.lua
@@ -0,0 +1,7 @@
+equipment = {
+	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+	{"object/tangible/wearables/shoes/shoes_s02.iff", },
+	{"object/tangible/wearables/shirt/shirt_s16.iff", },
+	{"object/tangible/wearables/pants/pants_s21.iff", },
+	{"object/tangible/wearables/armor/imp_army/armor_imp_army_chest_plate.iff", },
+}
