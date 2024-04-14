a23194b9093a02b63275e09f5b4f2010789fb356
Whiskey <RobinTh11@googlemail.com>
2024-04-14 19:35:10 +0200

    Update meditate.lua

--- MMOCoreORB/bin/scripts/screenplays/borrie/force/powers/meditate.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/force/powers/meditate.lua
@@ -8,7 +8,7 @@ BorForce_Meditate = BorForce_BasePower:new({
 
 	targetSelf = true,
 
-	helpString = "Restore half of your total Force Pool as a short rest. You must be out of combat, and not in imminent danger to do this.",
+	helpString = "Restore your Force Pool as a short rest. You must be out of combat, and not in imminent danger to do this.",
 })
 
 function BorForce_Meditate:showHelp(pPlayer)
@@ -45,17 +45,12 @@ function BorForce_Meditate:performAbility(pPlayer, fpi)
 		return
 	end
 	
-	local clientEffect = "clienteffect/pl_force_meditate_self.cef"
-	
 	local message = CreatureObject(pPlayer):getFirstName() .. " used " .. self.name .. "! In the cover of safety, they regenerate their force pool by " .. math.floor(forcePowerMax / 2) .. " points!"
 	
 	CreatureObject(pPlayer):setMoodString("meditating")
 	CreatureObject(pPlayer):setPosture(POSTURESITTING)
-
 	
-	local forcePowerPoints = math.min(forcePowerMax, forcePower + (forcePowerMax / 2))
-	
-	PlayerObject(pGhost):setForcePower(forcePowerPoints)
+	PlayerObject(pGhost):setForcePower(forcePowerMax)
 	SceneObject(pPlayer):setStoredInt("force_defense", 0)
 	CreatureObject(pPlayer):setHAM(6, CreatureObject(pPlayer):getHAM(6) - 1)
 	
