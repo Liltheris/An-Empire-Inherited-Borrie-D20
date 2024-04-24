44e463fabe5e69d0a9f59bac17e766b67cc33ecf
Whiskey <RobinTh11@googlemail.com>
2024-04-24 12:16:08 +0200

    Implemented functionality to some RP items

--- MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/items/rp_item_hydrospanner.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/items/rp_item_hydrospanner.lua
@@ -12,8 +12,29 @@ function BorRpItemHydrospannerMenuComponent:handleObjectMenuSelect(pObject, pPla
 		return 0
 	end
 	
-	if (selectedID == 20) then 
-		CreatureObject(pPlayer):sendSystemMessage("DEV: Work in progress, sorry! - Borrie")
+	if (selectedID == 20) then
+		-- Calculate the roll result first.
+		local skillValue = math.floor(CreatureObject(pPlayer):getSkillMod("rp_mechanics"))
+		local roll = math.floor(math.random(1,20))
+		local itemMod = 2
+		local result = skillValue + roll + itemMod
+
+		local rollString = ""
+
+		-- Critical highlighting on the roll
+		if(roll == 20) then
+			rollString = "\\#00FF00"..roll.."\\#FFFFFF"
+		elseif (roll == 1) then
+			rollString = "\\#FF0000"..roll.."\\#FFFFFF"
+		else
+			rollString = rollString..roll
+		end
+
+		-- Message output
+		local msg = CreatureObject(pPlayer):getFirstName().." uses a hydrospanner! Mechanics: 1d20: "..rollString.." + "..skillValue.." + \\#6666FF"..itemMod.."\\#FFFFFF"
+		msg = msg.." = "..result
+
+		broadcastMessageWithName(pPlayer, msg)
 	end
 	
 	return 0
--- MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/items/rp_item_security_spike.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/rp_items/items/rp_item_security_spike.lua
@@ -12,8 +12,33 @@ function BorRpItemSecuritySpikeMenuComponent:handleObjectMenuSelect(pObject, pPl
 		return 0
 	end
 	
-	if (selectedID == 20) then 
-		CreatureObject(pPlayer):sendSystemMessage("DEV: Work in progress, sorry! - Borrie")
+	if (selectedID == 20) then
+		-- Calculate the roll result first.
+		local skillValue = math.floor(CreatureObject(pPlayer):getSkillMod("rp_slicing"))
+		local roll = math.floor(math.random(1,20))
+		local itemMod = 3
+		local result = skillValue + roll + itemMod
+
+		local rollString = ""
+
+		-- Critical highlighting on the roll
+		if(roll == 20) then
+			rollString = "\\#00FF00"..roll.."\\#FFFFFF"
+		elseif (roll == 1) then
+			rollString = "\\#FF0000"..roll.."\\#FFFFFF"
+		else
+			rollString = rollString..roll
+		end
+
+		-- Message output
+		local msg = CreatureObject(pPlayer):getFirstName().." uses a security spike! Slicing: 1d20: "..rollString.." + "..skillValue.." + \\#6666FF"..itemMod.."\\#FFFFFF"
+		msg = msg.." = "..result
+
+		broadcastMessageWithName(pPlayer, msg)
+
+		-- Delete the spike!
+		SceneObject(pObject):destroyObjectFromWorld()
+		SceneObject(pObject):destroyObjectFromDatabase()
 	end
 	
 	return 0
