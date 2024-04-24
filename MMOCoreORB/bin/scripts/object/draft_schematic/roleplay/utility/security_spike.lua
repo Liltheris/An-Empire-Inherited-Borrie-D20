b31d3e66cfcb62706628e029e413109aa9e22bea
Valerie <liltheris@gmail.com>
2024-04-24 11:39:45 +0100

    Serverside implementation of spike crafting

---
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/utility/security_spike.lua
@@ -0,0 +1,35 @@
+arbineobject_draft_schematic_roleplay_utility_security_spike = object_draft_schematic_roleplay_utility_shared_security_spike:new {
+
+	templateType = DRAFTSCHEMATIC,
+
+	customObjectName = "Security Spike",
+
+	craftingToolTab = 1, -- (See DraftSchematicObjectTemplate.h)
+	complexity = 12,
+	size = 1,
+	factoryCrateType = "object/factory/factory_crate_weapon.iff",
+
+	xpType = "",
+	xp = 0,
+
+	assemblySkill = "",
+	experimentingSkill = "",
+	customizationSkill = "",
+ 	factoryCrateSize = 0,
+
+	customizationOptions = {},
+	customizationStringNames = {},
+	customizationDefaults = {},
+
+	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
+	ingredientTitleNames = {"computer_spike","circuit_board"},
+	ingredientSlotType = {1,1},
+	resourceTypes = {"object/tangible/borrp/item/computer_spike.iff","object/tangible/borrp/crafting/shared_comp_circuit_board.iff"},
+	resourceQuantities = {1,1},
+	contribution = {100,100},
+
+	targetTemplate = "object/borrp/item/security_spike.iff",
+
+	additionalTemplates = {}
+
+}ObjectTemplates:addTemplate(object_draft_schematic_roleplay_utility_security_spike, "object/draft_schematic/roleplay/utility/security_spike.iff")
---
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/utility/serverobjects.lua
@@ -0,0 +1,2 @@
+includeFile("draft_schematic/roleplay/utility/computer_spike.lua")
+includeFile("draft_schematic/roleplay/utility/security_spike.lua")
