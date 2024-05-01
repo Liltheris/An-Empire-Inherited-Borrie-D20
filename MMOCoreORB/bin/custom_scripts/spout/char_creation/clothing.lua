f7de7fe5d986f9580aa6a49bbea07d46f65b8625
Valerie <liltheris@gmail.com>
2024-05-01 09:38:26 +0100

    pushback

---
+++ MMOCoreORB/bin/custom_scripts/spout/char_creation/clothing.lua
@@ -0,0 +1,1 @@
+spawnSceneObject("tutorial", "object/tangible/sign/all_sign_city_s03.iff", 26.0151, -4.2, -152.784, 694200001, 1, 0, 0, 0)
---
+++ MMOCoreORB/bin/custom_scripts/spout/char_creation/weapons.lua
@@ -0,0 +1,1 @@
+spawnSceneObject("tutorial", "object/tangible/sign/all_sign_city_s03.iff", 28.9283, -4.2, -152.784, 694200001, 1, 0, 0, 0)
