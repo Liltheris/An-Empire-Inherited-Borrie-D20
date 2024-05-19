a18bf1a44003910fef4e8565fe7aa2e91eda1f35
Valerie <liltheris@gmail.com>
2024-05-19 17:48:44 +0100

    it didnae work

--- MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/rifle/rifle_bowcaster.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/rifle/rifle_bowcaster.lua
@@ -21,13 +21,13 @@ object_draft_schematic_roleplay_weapon_rifle_rifle_bowcaster = object_draft_sche
 	customizationStringNames = {},
 	customizationDefaults = {},
 
-	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
-	ingredientTitleNames = {"receiver","operating_system","scope", "muzzle"},
-	ingredientSlotType = {1,1,3,3},
-	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_rifle_common.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff","object/tangible/component/weapon/shared_scope_weapon.iff","object/tangible/component/weapon/shared_blaster_rifle_barrel.iff"},
-	resourceQuantities = {1,1,1,1},
-	contribution = {100,100,100,100},
-	ingredientAppearance = {"", "", "scope","muzzle"},
+	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
+	ingredientTitleNames = {"receiver","operating_system","scope"},
+	ingredientSlotType = {1,1,3},
+	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_rifle_common.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff","object/tangible/component/weapon/shared_scope_weapon.iff"},
+	resourceQuantities = {1,1,1},
+	contribution = {100,100,100},
+	ingredientAppearance = {"", "", "scope"},
 	targetTemplate = "object/weapon/roleplay/ranged/rifle/bowcaster.iff",
 
 	additionalTemplates = {}
