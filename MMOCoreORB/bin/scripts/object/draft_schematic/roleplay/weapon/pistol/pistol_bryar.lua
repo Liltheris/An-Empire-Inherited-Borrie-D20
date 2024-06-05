ea1c6a162b7a7024afa5f20a0c37e2ffcffce791
Valerie <liltheris@gmail.com>
2024-06-05 18:01:20 +0100

    Didn't save a file x

--- MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/pistol_bryar.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/roleplay/weapon/pistol/pistol_bryar.lua
@@ -1,8 +1,8 @@
-object_draft_schematic_roleplay_weapon_pistol_pistol_cdef = object_draft_schematic_roleplay_weapon_pistol_shared_pistol_cdef:new {
+object_draft_schematic_roleplay_weapon_pistol_pistol_bryar = object_draft_schematic_roleplay_weapon_pistol_shared_pistol_bryar:new {
 
 	templateType = DRAFTSCHEMATIC,
 
-	customObjectName = "CDEF Blaster Pistol",
+	customObjectName = "D-18 Blaster Pistol",
 
 	craftingToolTab = 1, -- (See DraftSchematicObjectTemplate.h)
 	complexity = 8,
@@ -21,14 +21,15 @@ object_draft_schematic_roleplay_weapon_pistol_pistol_cdef = object_draft_schemat
 	customizationStringNames = {},
 	customizationDefaults = {},
 
-	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
-	ingredientTitleNames = {"receiver","operating_system"},
-	ingredientSlotType = {1,1},
-	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_pistol_common.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff"},
-	resourceQuantities = {1,1},
-	contribution = {100,100},
-	targetTemplate = "object/weapon/roleplay/ranged/pistol/cdef_pistol.iff",
+	ingredientTemplateNames = {"craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n","craft_weapon_ingredients_n"},
+	ingredientTitleNames = {"receiver","operating_system","barrel","scope"},
+	ingredientSlotType = {1,1,3,3},
+	resourceTypes = {"object/tangible/borrp/crafting/shared_comp_weapon_receiver_pistol_common.iff","object/tangible/borrp/crafting/shared_comp_op_system_base.iff","object/tangible/component/weapon/shared_blaster_pistol_barrel.iff","object/tangible/component/weapon/shared_scope_weapon.iff"},
+	resourceQuantities = {1,1,1,1},
+	contribution = {100,100,100,100},
+	ingredientAppearance = {"", "", "muzzle", "scope"},
+	targetTemplate = "object/weapon/roleplay/ranged/pistol/bryar_pistol.iff",
 
 	additionalTemplates = {}
 
-}ObjectTemplates:addTemplate(object_draft_schematic_roleplay_weapon_pistol_pistol_cdef, "object/draft_schematic/roleplay/weapon/pistol/pistol_cdef.iff")
+}ObjectTemplates:addTemplate(object_draft_schematic_roleplay_weapon_pistol_pistol_bryar, "object/draft_schematic/roleplay/weapon/pistol/pistol_bryar.iff")
