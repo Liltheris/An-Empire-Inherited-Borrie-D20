2b53160ddbf66b7de247a4dc217f740b581f1b63
Valerie <liltheris@gmail.com>
2024-06-10 10:29:09 +0100

    It's about time I spawned Wicha

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/tatooine/anchorhead/wicha.lua
@@ -0,0 +1,9 @@
+equipment = {
+	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+	{"object/tangible/wearables/jacket/jacket_s48.iff", },
+	{"object/tangible/wearables/pants/pants_s08.iff", "/private/index_color_1", 242, },
+	{"object/tangible/wearables/shoes/shoes_s07.iff", },
+	{"object/tangible/wearables/bandolier/bandolier_s09.iff", },
+	{"object/tangible/wearables/shirt/shirt_s12.iff", },
+	{"object/tangible/wearables/hat/hat_s14.iff", "/private/index_color_1", 200, },
+}
