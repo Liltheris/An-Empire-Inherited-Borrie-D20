a92ac8d8b02281ed12327b86b01829b1341fcddb
Valerie <liltheris@gmail.com>
2024-06-15 23:27:56 +0100

    Pusback of npc placement 2

---
+++ MMOCoreORB/bin/custom_scripts/spout/coruscant/estate_rpmob.lua
@@ -0,0 +1,6 @@
+spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 8.62898, 0, -102.244, 83, 0, "object/mobile/rp_zeltron_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, -6.98502, 0, -88.1737, 91, 0, "object/mobile/rp_twilek_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 3.06569, 1.13306, -13.1405, 284, 880033011, "object/mobile/rp_twilek_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, -12.7037, 1.13306, -7.5, 167, 880033012, "object/mobile/rp_zeltron_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 6.15677, 1.13306, 1.24026, 257, 880033007, "object/mobile/rp_twilek_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 21.7183, 0, -125.992, 262, 0, "object/mobile/rp_twilek_female.iff", "default", "default", "default") --Equip, Skill, Customization
