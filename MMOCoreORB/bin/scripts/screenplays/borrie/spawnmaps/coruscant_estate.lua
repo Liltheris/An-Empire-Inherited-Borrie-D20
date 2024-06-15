78a959c938ffc7b1e83eef6e890dbea2895dfe80
Whiskey <RobinTh11@googlemail.com>
2024-06-16 00:34:21 +0200

    Update coruscant_estate.lua

--- MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/coruscant_estate.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/spawnmaps/coruscant_estate.lua
@@ -199,7 +199,16 @@ end
 
 function CoruscantEstateScreenPlay:spawnMobiles()
 	--Species, Equipment, Skills, Customization
-	
-
-
+	local npc = spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 8.62898, 0, -102.244, 83, 0, "rp_zeltron_female", "event/estate/server", "civilian", "random") --Equip, Skill, Customization
+	SceneObject(npc):setCustomObjectName("a Serving Girl")
+	npc = spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, -6.98502, 0, -88.1737, 91, 0, "rp_twilek_female", "event/estate/server", "civilian", "random") --Equip, Skill, Customization
+	SceneObject(npc):setCustomObjectName("a Serving Girl")
+	npc = spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 3.06569, 1.13306, -13.1405, 284, 880033011, "rp_twilek_female", "event/estate/server", "civilian", "random") --Equip, Skill, Customization
+	SceneObject(npc):setCustomObjectName("a Serving Girl")
+	npc = spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, -12.7037, 1.13306, -7.5, 167, 880033012, "rp_zeltron_female", "event/estate/server", "civilian", "random") --Equip, Skill, Customization
+	SceneObject(npc):setCustomObjectName("a Serving Girl")
+	npc = spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 6.15677, 1.13306, 1.24026, 257, 880033007, "rp_twilek_female", "event/estate/server", "civilian", "random") --Equip, Skill, Customization
+	SceneObject(npc):setCustomObjectName("a Serving Girl")
+	npc = spawnRoleplayMobile("rp_coruscant", "rp_base_npc", 1, 21.7183, 0, -125.992, 262, 0, "rp_twilek_female", "event/estate/server", "civilian", "random") --Equip, Skill, Customization
+	SceneObject(npc):setCustomObjectName("a Serving Girl")
 end
