63c3fe6b2cedbf826533d8c17957a2667f2a651a
Whiskey <RobinTh11@googlemail.com>
2024-06-30 16:47:05 +0200

    Thanks C++

--- MMOCoreORB/src/server/zone/objects/creature/commands/FireHeavyWeaponCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/FireHeavyWeaponCommand.h
@@ -5,6 +5,8 @@
 #ifndef FIREHEAVYWEAPONCOMMAND_H_
 #define FIREHEAVYWEAPONCOMMAND_H_
 
+#include "server/zone/borrie/BorCombat.h"
+
 class FireHeavyWeaponCommand : public CombatQueueCommand {
 public:
 
