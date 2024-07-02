64f79183ac28cc605f208520ae9206b7756f2608
Valerie <liltheris@gmail.com>
2024-07-02 22:49:48 +0100

    i forgor some files

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/gar/cor_guard/commander.lua
@@ -0,0 +1,15 @@
+equipment = {
+	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_helmet.iff", "/private/index_color_2", 0, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_chest_plate.iff", "/private/index_color_2", 0, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/clone_trooper/armor_clone_trooper_neutral_s01_belt.iff", },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_boots.iff", "/private/index_color_2", 0, "/private/index_color_1", 0, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_leggings.iff", "/private/index_color_2", 0, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_bicep_r.iff", "/private/index_color_2", 0, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_bicep_l.iff", "/private/index_color_2", 0, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_gloves.iff", "/private/index_color_2", 13, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_bracer_r.iff", "/private/index_color_2", 13, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_bracer_l.iff", "/private/index_color_2", 13, "/private/index_color_1", 22, "/private/index_texture_1", 4, },
+	{"object/tangible/wearables/cape/cape_kama_s02.iff", "/private/index_color_2", 6, "/private/index_color_1", 6, "/private/index_color_3", 6, "/private/index_texture_1", 2, },
+	{"object/tangible/wearables/pauldron/pauldron_s01.iff", "/private/index_color_2", 1, },
+}
