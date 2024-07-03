f527cb2e9d2249a9e9f2fd0bbacc2b266c0b6b83
Valerie <liltheris@gmail.com>
2024-07-03 10:43:28 +0100

    pushback for test

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/testnpc.lua
@@ -0,0 +1,5 @@
+equipment = {
+	{"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_chest_plate.iff", "/shared_owner/blend_flat_chest", 0, },
+	{"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_leggings.iff", "/shared_owner/blend_fat", 0, },
+	{"object/tangible/wearables/armor/roleplay/stormtrooper/armor_stormtrooper_leggings.iff", "/shared_owner/blend_fat", 0, },
+}
