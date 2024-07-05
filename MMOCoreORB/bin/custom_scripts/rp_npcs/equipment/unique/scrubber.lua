cc002356e993c1d61def311b5a819009113c6db0
Valerie <liltheris@gmail.com>
2024-07-05 10:42:59 +0100

    pushback

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/unique/scrubber.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/unique/scrubber.lua
@@ -1,9 +1,9 @@
 equipment = {
-	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
-	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_chest_plate.iff", "/private/index_color_2", 14, },
-	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_leggings.iff", "/private/index_color_2", 14, },
-	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_boots.iff", "/private/index_color_2", 14, },
-	{"object/tangible/wearables/armor/clone_trooper/armor_clone_trooper_neutral_s01_belt.iff", },
 	{"object/tangible/wearables/shirt/shirt_s04.iff", "/private/index_color_1", 5, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_chest_plate.iff", "/private/index_color_2", 16, },
 	{"object/tangible/wearables/gloves/gloves_s12.iff", "/private/index_color_1", 3, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_leggings.iff", "/private/index_color_2", 16, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_leggings.iff", "/private/index_color_2", 16, },
+	{"object/tangible/wearables/armor/roleplay/clone_trooper/armor_clone_trooper_neutral_s01_boots.iff", "/private/index_color_2", 16, },
+	{"object/tangible/wearables/armor/clone_trooper/armor_clone_trooper_neutral_s01_belt.iff", },
 }
