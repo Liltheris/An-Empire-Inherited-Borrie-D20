1218c7403efc721a34afebb36096cde26a81e805
Valerie <liltheris@gmail.com>
2024-07-12 11:35:52 +0100

    pushback

---
+++ MMOCoreORB/bin/custom_scripts/structure_prefabs/TeTakki.lua
@@ -0,0 +1,21 @@
+--Prefab created by Sage
+--Intended for Building Template: object/building/player_sp/sandcrawler_house.iff
+prefab = {
+	{ "object/tangible/swsovrp/prop2/poi_r4_powerdown.iff", "A Defunct R4 Droid", 3, 5.66966, 9.79876, 4.12332, 0.707107, 0, -0.707107, 0 },
+	{ "object/tangible/swsovrp/prop2/eqp_tauntaun_ride_base.iff", "", 3, -5.66053, 9.29876, -2.18223, 1, 0, 0, 0 },
+	{ "object/tangible/swsovrp/prop2/eqp_tauntaun_ride_mount.iff", "", 3, -5.66053, 9.29876, -2.18223, 1, 0, 0, 0 },
+	{ "object/tangible/swsovrp/prop/xwing_debris_c.iff", "", 3, 5.80445, 13.2988, 4.49329, 1, 0, 0, 0 },
+	{ "object/tangible/furniture/all/frn_all_desk_radar_topology_screen.iff", "", 3, 4.92505, 9.29876, 11.4809, 0.833886, 0, 0.551937, 0 },
+	{ "object/tangible/swsovrp/prop/poi_all_atst_destroyed.iff", "AT-ST Cockpit", 3, 0.0271566, 14.2988, 1.03927, 0.991445, 0.130526, 0, 0 },
+	{ "object/tangible/storyteller/prop/pr_palette_supply_01.iff", "", 3, 3.68216, 9.29876, -3.98863, 1, 0, 0, 0 },
+	{ "object/tangible/furniture/all/frn_all_meatlump_grill_s01.iff", "", 3, 5.71346, 8.79876, -1.52382, 0.707107, 0, 0.707107, 0 },
+	{ "object/tangible/furniture/technical/cyborg_repair_table.iff", "", 3, 5.71346, 8.79876, 4.4019, 0.707107, 0, 0.707107, 0 },
+	{ "object/tangible/furniture/all/frn_all_rug_bantha_skin.iff", "", 3, 1.72066, 13.7988, -5.32201, 0.707107, 0.707107, 0, 0 },
+	{ "object/tangible/furniture/space/frn_chair_falcon_passenger_s01.iff", "", 3, 0.494032, 17.6871, 24.3722, 1, 0, 0, 0 },
+	{ "object/tangible/furniture/space/frn_chair_falcon_passenger_s01.iff", "", 3, -0.505968, 17.6871, 24.3722, 1, 0, 0, 0 },
+	{ "object/tangible/furniture/tatooine/uber_must_mining_console_s01.iff", "Controls", 3, 0.0442619, 17.1871, 25.8722, 1, 0, 0, 0 },
+	{ "object/tangible/furniture/tatooine/uber_must_mining_console_s02.iff", "Controls", 3, -1.95578, 17.1871, 25.3723, 0.92388, 0, -0.382683, 0 },
+	{ "object/tangible/furniture/tatooine/uber_must_mining_console_s02.iff", "Controls", 3, 2.04422, 17.1871, 25.3721, 0.92388, 0, 0.382683, 0 },
+	{ "object/weapon/ranged/rifle/rifle_geo_drill.iff", "Ancient Mining Equipment", 3, -5.06011, 9.79876, -4.84685, 0.653281, 0.270598, 0.653281, -0.270598 },
+}
+
