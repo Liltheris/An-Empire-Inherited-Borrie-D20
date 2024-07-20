c81ab552d4ce6ab6c5db80823ab45be0a77188c5
Whiskey <RobinTh11@googlemail.com>
2024-07-20 13:55:44 +0200

    Stormtroper pauldron (server)

--- MMOCoreORB/bin/scripts/object/tangible/wearables/pauldron/objects.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/pauldron/objects.lua
@@ -7,3 +7,10 @@ object_tangible_wearables_pauldron_shared_pauldron_s02 = SharedTangibleObjectTem
 	clientTemplateFileName = "object/tangible/wearables/pauldron/shared_pauldron_s02.iff"
 }
 ObjectTemplates:addClientTemplate(object_tangible_wearables_pauldron_shared_pauldron_s02, "object/tangible/wearables/pauldron/shared_pauldron_s02.iff")
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+object_tangible_wearables_pauldron_shared_pauldron_s03 = SharedTangibleObjectTemplate:new {
+	clientTemplateFileName = "object/tangible/wearables/pauldron/shared_pauldron_s03.iff"
+}
+ObjectTemplates:addClientTemplate(object_tangible_wearables_pauldron_shared_pauldron_s03, "object/tangible/wearables/pauldron/shared_pauldron_s03.iff")
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
