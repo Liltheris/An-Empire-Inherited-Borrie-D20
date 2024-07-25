15a622e95327d181de5c9f623ed87ce5c49d85eb
Whiskey <RobinTh11@googlemail.com>
2024-07-25 11:47:59 +0200

    added new templates

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/lukasene/ceremonial_guard.lua
@@ -0,0 +1,20 @@
+npc_template = {
+
+	randomName = "zabrak",
+	
+	baseTemplates = {
+		"rp_miraluka_male", 50,
+		"rp_miraluka_female", 50
+	},
+	skillTemplates = {"force/trained"},
+	
+	equipmentTemplates = {
+		{"rp_miraluka_male", {"faction/lukasene/seeker_ceremonial_s01","faction/lukasene/seeker_ceremonial_s02","faction/lukasene/seeker_ceremonial_s03","faction/lukasene/seeker_ceremonial_s04", "faction/lukasene/seeker_ceremonial_s05"}},
+		{"rp_miraluka_female", {"faction/lukasene/seeker_ceremonial_s01","faction/lukasene/seeker_ceremonial_s02","faction/lukasene/seeker_ceremonial_s03","faction/lukasene/seeker_ceremonial_s04", "faction/lukasene/seeker_ceremonial_s05"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+	},	
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/newrepublic/senetate_guard.lua
@@ -0,0 +1,45 @@
+npc_template = {
+
+	randomName = "human",
+	
+	baseTemplates = {
+		"rp_human_male", 50, "rp_human_female", 30,
+		"rp_sullustan_male", 5, "rp_sullustan_female", 2,
+		"rp_zabrak_male", 15, "rp_zabrak_female", 15,
+		"rp_mirialan_male", 10, "rp_mirialan_female", 10,
+		"rp_moncal_male", 5, "rp_moncal_female", 2,
+		"rp_twilek_male", 15, "rp_twilek_female", 10,
+	},
+	
+	skillTemplates = {"common/soldier_strong"},
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/newrepublic/senate_guard"}},
+		{"rp_human_female", {"faction/newrepublic/senate_guard"}},
+		{"rp_sullustan_male", {"faction/newrepublic/senate_guard"}},
+		{"rp_sullustan_female", {"faction/newrepublic/senate_guard"}},
+		{"rp_zabrak_male", {"faction/newrepublic/senate_guard"}},
+		{"rp_zabrak_female", {"faction/newrepublic/senate_guard"}},
+		{"rp_mirialan_male", {"faction/newrepublic/senate_guard"}},
+		{"rp_mirialan_female", {"faction/newrepublic/senate_guard"}},
+		{"rp_moncal_male", {"faction/newrepublic/senate_guard"}},
+		{"rp_moncal_female", {"faction/newrepublic/senate_guard"}},
+		{"rp_twilek_male", {"faction/newrepublic/senate_guard"}},
+		{"rp_twilek_female", {"faction/newrepublic/senate_guard"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random_military"}},
+		{"rp_human_female", {"random_military"}},
+		{"rp_sullustan_male", {"random_military"}},
+		{"rp_sullustan_female", {"random_military"}},
+		{"rp_zabrak_male", {"random_military"}},
+		{"rp_zabrak_female", {"random_military"}},
+		{"rp_mirialan_male", {"random_military"}},
+		{"rp_mirialan_female", {"random_military"}},
+		{"rp_moncal_male", {"random_military"}},
+		{"rp_moncal_female", {"random_military"}},
+		{"rp_twilek_male", {"random_military"}},
+		{"rp_twilek_female", {"random_military"}},
+	},	
+}
