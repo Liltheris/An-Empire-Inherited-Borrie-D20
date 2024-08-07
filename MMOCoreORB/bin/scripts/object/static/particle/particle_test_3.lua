0b685401ac055bab08fcf6eb3a94d3484cf0b9d8
Whiskey <RobinTh11@googlemail.com>
2024-08-07 15:33:25 +0200

    Create particle_test_3.lua

---
+++ MMOCoreORB/bin/scripts/object/static/particle/particle_test_3.lua
@@ -0,0 +1,5 @@
+object_static_particle_particle_test_3 = object_static_particle_shared_particle_test_3:new {
+
+}
+
+ObjectTemplates:addTemplate(object_static_particle_particle_test_3, "object/static/particle/particle_test_3.iff")
