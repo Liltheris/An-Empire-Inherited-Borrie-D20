96545912c2a4a12c64ed3b46ba58d1c45f932d4c
Valerie <liltheris@gmail.com>
2024-08-08 10:44:29 +0100

    moar pushback

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/planet/naboo/bank_teller_female.lua
@@ -0,0 +1,6 @@
+equipment = {
+	{"object/tangible/wearables/shirt/shirt_s32.iff", "/private/index_color_2", 45, "/private/index_color_1", 6, },
+	{"object/tangible/wearables/skirt/skirt_s14.iff", "/private/index_color_2", 6, "/private/index_color_1", 1, },
+	{"object/tangible/wearables/skirt/skirt_s14.iff", "/private/index_color_2", 6, "/private/index_color_1", 1, },
+	{"object/tangible/wearables/shoes/shoes_s09.iff", "/private/index_color_1", 6, },
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/planet/naboo/bank_teller_male.lua
@@ -0,0 +1,5 @@
+equipment = {
+	{"object/tangible/wearables/shirt/shirt_s32.iff", "/private/index_color_2", 45, "/private/index_color_1", 6, },
+	{"object/tangible/wearables/pants/pants_s13.iff", "/private/index_color_1", 6, },
+	{"object/tangible/wearables/shoes/shoes_s01.iff", },
+}
