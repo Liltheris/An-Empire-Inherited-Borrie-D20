3275767da35c147ca9dc69d44d13b64ccce5ba7a
Valerie <liltheris@gmail.com>
2024-08-18 23:12:32 +0100

    woops 2

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/imperial/scout_camo.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/imperial/scout_camo.lua
@@ -1,9 +1,9 @@
 equipment = {
-	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bicep_l_camo.iff", },
-	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bicep_r_camo.iff", },
+	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bicep_camo_l.iff", },
+	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bicep_camo_r.iff", },
 	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_boots_camo.iff", },
-	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bracer_l_camo.iff", },
-	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bracer_r_camo.iff", },
+	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bracer_camo_l.iff", },
+	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_bracer_camo_r.iff", },
 	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_chest_plate_camo.iff", },
 	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_gloves_camo.iff", },
 	{"object/tangible/wearables/armor/roleplay/scout_trooper/shared_armor_scout_trooper_helmet_camo.iff", },
