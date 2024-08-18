7ed9a46f9481636cba24da8904b2d4c6f4a0b307
Valerie <liltheris@gmail.com>
2024-08-18 13:19:54 +0100

    Pushback of live

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/inquisitor/officer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/inquisitor/officer.lua
@@ -1,8 +1,13 @@
 equipment = {
-	{"object/weapon/melee/unarmed/unarmed_default_player.iff", "/private/index_color_blade", 0, },
-	{"object/tangible/wearables/gloves/gloves_s12.iff", "/private/index_color_1", 121, },
-	{"object/tangible/wearables/pants/pants_s34.iff", "/private/index_color_1", 20, },
-	{"object/tangible/wearables/boots/boots_s12.iff", "/private/index_color_1", 199, },
 	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
-	{"object/tangible/wearables/hat/hat_imp_s01.iff", "/private/index_color_2", 0, "/private/index_color_1", 16, },
+	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/gloves/gloves_s02.iff", "/private/index_color_1", 31, },
+	{"object/tangible/wearables/hat/hat_imp_s01.iff", "/private/index_color_1", 43, },
+	{"object/tangible/wearables/pants/pants_s34.iff", "/private/index_color_1", 16, },
+	{"object/tangible/wearables/boots/boots_s12.iff", "/private/index_color_1", 96, },
 }
