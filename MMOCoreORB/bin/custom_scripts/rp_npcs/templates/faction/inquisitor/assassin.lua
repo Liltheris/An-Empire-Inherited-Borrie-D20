ce913fdca505efd96be5730dfe9020fea5b2b381
Valerie <liltheris@gmail.com>
2024-08-21 12:14:19 +0100

    :eyes:

--- MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/inquisitor/assassin.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/inquisitor/assassin.lua
@@ -1,6 +1,6 @@
 npc_template = {
 
-	Name = "???",
+	name = "???",
 	baseTemplates = {
 		"rp_human_male", 90,
 		"rp_human_female", 10
@@ -21,6 +21,7 @@ npc_template = {
 		{"/shared_owner/blend_skinny", 71},
 		{"/shared_owner/blend_muscle", 0},
 		{"/shared_owner/blend_fat", 0},
+		{"/private/index_color_2", 7},
 		{"height", 1.3},
 	},
 
