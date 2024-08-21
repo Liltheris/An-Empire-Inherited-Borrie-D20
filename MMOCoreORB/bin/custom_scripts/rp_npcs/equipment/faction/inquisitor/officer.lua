99da4fbedafa55fb31fe0894c3d4e38d74d73260
Valerie <liltheris@gmail.com>
2024-08-21 12:02:03 +0100

    Pushback :3

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/inquisitor/officer.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/inquisitor/officer.lua
@@ -7,7 +7,7 @@ equipment = {
 	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
 	{"object/tangible/wearables/jacket/jacket_s03.iff", "/private/index_color_1", 43, },
 	{"object/tangible/wearables/gloves/gloves_s02.iff", "/private/index_color_1", 31, },
-	{"object/tangible/wearables/hat/hat_imp_s01.iff", "/private/index_color_1", 43, },
-	{"object/tangible/wearables/pants/pants_s34.iff", "/private/index_color_1", 16, },
+	{"object/tangible/wearables/hat/hat_imp_s01.iff", "/private/index_color_1", 16, },
+	{"object/tangible/wearables/pants/pants_s34.iff", "/private/index_color_1", 30, },
 	{"object/tangible/wearables/boots/boots_s12.iff", "/private/index_color_1", 96, },
 }
