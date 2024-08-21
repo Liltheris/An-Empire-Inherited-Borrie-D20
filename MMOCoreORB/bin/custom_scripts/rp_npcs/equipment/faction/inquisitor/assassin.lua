800e6644ed2f483c8a6e116b76136cf2a947164e
Valerie <liltheris@gmail.com>
2024-08-21 12:07:38 +0100

    Fixed the Inquisitor Assassins outfit & Template

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/inquisitor/assassin.lua
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/faction/inquisitor/assassin.lua
@@ -1,12 +1,5 @@
 equipment = {
 	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/jacket/jacket_tcg_s01.iff", "/private/index_color_1", 90, },
-	{"object/tangible/wearables/cape/cape_kama_s02.iff", "/private/index_color_2", 132, "/private/index_color_1", 90, "/private/index_color_3", 41, "/private/index_texture_1", 1, },
 	{"object/tangible/wearables/cape/cape_kama_s02.iff", "/private/index_color_2", 132, "/private/index_color_1", 90, "/private/index_color_3", 41, "/private/index_texture_1", 1, },
 	{"object/tangible/wearables/mask/mask_sith_s01.iff", "/private/index_color_2", 90, },
 	{"object/tangible/wearables/gloves/gloves_s02.iff", "/private/index_color_1", 31, },
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/inquisitor/assassin.lua
@@ -0,0 +1,27 @@
+npc_template = {
+
+	Name = "???",
+	baseTemplates = {
+		"rp_human_male", 90,
+		"rp_human_female", 10
+	},
+	skillTemplates = {"narestra/force/adept"},
+	
+	equipmentTemplates = {
+		{"rp_human_male", {"faction/inquisitor/assassin"}},
+		{"rp_human_female", {"faction/inquisitor/assassin"}},
+	},	
+	
+	customizationTemplates = {
+		{"rp_human_male", {"random"}},
+		{"rp_human_female", {"random"}},
+	},	
+	
+	customVarOverrides = {
+		{"/shared_owner/blend_skinny", 71},
+		{"/shared_owner/blend_muscle", 0},
+		{"/shared_owner/blend_fat", 0},
+		{"height", 1.3},
+	},
+
+}
