efbfefa55197f489ef5987b29550acb5784e5b19
Whiskey <RobinTh11@googlemail.com>
2024-09-24 15:12:20 +0200

    Added G9 Rigger to ship vendors

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_ship_cheap.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_ship_cheap.lua
@@ -20,6 +20,7 @@ rpg_shop_general = {
 			name = "Freighters",
 			minItems = 2,
 			items = {
+				{template = "object/tangible/deed/rp_ship_deed/rp_g9_deed.iff", cost = 12500, max = 10, chance = 100, overridePrice = true},
 				{template = "object/tangible/deed/rp_ship_deed/rp_yt1300_deed.iff", cost = 18000, max = 10, chance = 100 , overridePrice = true},
 				{template = "object/tangible/deed/rp_ship_deed/rp_yt2400_deed.iff", cost = 20000, max = 10, chance = 100, overridePrice = true },
 			}
