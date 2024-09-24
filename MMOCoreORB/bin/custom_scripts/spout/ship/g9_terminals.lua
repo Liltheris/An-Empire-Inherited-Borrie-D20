40c9cf0ed8db51f643fdf786504e6cb0fd48e427
Valerie <liltheris@gmail.com>
2024-09-24 12:35:11 +0100

    Pushback of test

---
+++ MMOCoreORB/bin/custom_scripts/spout/ship/g9_terminals.lua
@@ -0,0 +1,3 @@
+spawnSceneObject("rp_ship_a", "object/tangible/ship/roleplay/terminal_navicomputer.iff", -0.0398465, 6.83227, 7.35068, 281474998678687, -4.37114e-08, 0, 1, 0)
+spawnSceneObject("rp_ship_a", "object/tangible/terminal/terminal_player_structure.iff", -3.15717, 3.62095, -1.28922, 281474998678688, -4.37114e-08, 0, 1, 0)
+spawnSceneObject("rp_ship_a", "object/tangible/ship/roleplay/terminal_ship_exit.iff", -0.797393, 3.92095, -1.18144, 281474998678688, 0.707107, 0, 0.707107, 0)
