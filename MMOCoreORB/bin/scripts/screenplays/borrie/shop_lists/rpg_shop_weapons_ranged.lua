89db10932d0ffd927d1c9d27e381a961d21a2873
Whiskey <RobinTh11@googlemail.com>
2024-09-24 16:06:23 +0200

    added RK-3 pistol to vendors

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_weapons_ranged.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_weapons_ranged.lua
@@ -9,6 +9,7 @@ shop_weapons_ranged = {
 				{template = "object/weapon/roleplay/ranged/pistol/flare_gun.iff", cost = 100, max = 10, chance = 100 },
 				{template = "object/weapon/roleplay/ranged/pistol/ion_pistol.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/weapon/roleplay/ranged/pistol/d18_pistol.iff", cost = 350, max = 10, chance = 100 },
+				{template = "object/weapon/roleplay/ranged/pistol/pistol_rk3.iff", cost = 400, max = 10, chance = 100 },
 				{template = "object/weapon/roleplay/ranged/pistol/dl44_blaster_pistol.iff", cost = 400, max = 10, chance = 100 },
 				{template = "object/weapon/roleplay/ranged/pistol/dl44_metal_blaster_pistol.iff", cost = 450, max = 10, chance = 100 },
 				{template = "object/weapon/roleplay/ranged/pistol/power_5_heavy_blaster_pistol.iff", cost = 900, max = 10, chance = 100 },
