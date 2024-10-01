5cd6180c024f19a9e72f1fdd7716afc05e3536f6
Valerie <liltheris@gmail.com>
2024-10-01 12:10:11 +0100

    Pushback :3

---
+++ MMOCoreORB/bin/custom_scripts/spout/planet/naboo/travel_terminals.lua
@@ -0,0 +1,2 @@
+spawnSceneObject("rp_naboo", "object/tangible/tcg/series7/garage_display_vehicles/speederbike.iff", 5348.21, 326.984, -1552.06, 0, 0.0871558, 0, 0.996195, 0)
+spawnSceneObject("rp_naboo", "object/tangible/storyteller/prop/pr_clone_barc_speeder.iff", 4952.3, 4.34863, -4929.29, 0, 0.707107, 0, 0.707107, 0)
