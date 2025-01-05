9e699c4f06d96d8c7b4f19354b11c3dbb3e86d78
Valerie <liltheris@gmail.com>
2025-01-05 16:01:53 +0000

    Added flash speeder as an available speeder to buy. Two seater :D

--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_speeders.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_speeders.lua
@@ -7,10 +7,11 @@ rpg_shop_speeders = {
 			minItems = 1,
 			items = {				
 				{template = "object/tangible/deed/vehicle_deed/speederbike_deed.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
-				{template = "object/tangible/deed/vehicle_deed/landspeeder_xp38_deed.iff", cost = 500, max = 10, chance = 100, overridePrice = true },		
+				{template = "object/tangible/deed/vehicle_deed/landspeeder_xp38_deed.iff", cost = 500, max = 10, chance = 100, overridePrice = true },	
 				{template = "object/tangible/deed/vehicle_deed/landspeeder_ab1_deed.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
 				{template = "object/tangible/tcg/series5/vehicle_deed_air2_swoop_speeder.iff", cost = 800, max = 10, chance = 100, overridePrice = true },
 				{template = "object/tangible/deed/vehicle_deed/landspeeder_x34_deed.iff", cost = 600, max = 10, chance = 100, overridePrice = true },
+				{template = "object/tangible/deed/vehicle_deed/speederbike_flash_deed.iff", cost = 850, max = 10, chance = 100, overridePrice = true },	
 				{template = "object/tangible/deed/vehicle_deed/flare_s_swoop.iff", cost = 1500, max = 10, chance = 100, overridePrice = true },
 				{template = "object/tangible/deed/vehicle_deed/speederbike_swoop_deed.iff", cost = 1500, max = 10, chance = 100, overridePrice = true },
 				--{template = "object/tangible/deed/vehicle_deed/landspeeder_v35_deed.iff", cost = 6500, max = 10, chance = 100, overridePrice = true },		
