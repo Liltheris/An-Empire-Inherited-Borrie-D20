da7a2344512b7b70550969430c533d7ecf7b0160
Valerie <liltheris@gmail.com>
2025-01-15 16:05:24 +0000

    let's try again

--- MMOCoreORB/bin/scripts/object/tangible/wearables/goggles/goggles_kubaz_s02.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/goggles/goggles_kubaz_s02.lua
@@ -1,4 +1,4 @@
-object_tangible_wearables_goggles_goggles_kubaz_s01 = object_tangible_wearables_goggles_shared_goggles_kubaz_s01:new {
+object_tangible_wearables_goggles_goggles_kubaz_s02 = object_tangible_wearables_goggles_shared_goggles_kubaz_s02:new {
 
 }
-ObjectTemplates:addTemplate(object_tangible_wearables_goggles_goggles_kubaz_s01, "object/tangible/wearables/goggles/goggles_kubaz_s01.iff")
+ObjectTemplates:addTemplate(object_tangible_wearables_goggles_goggles_kubaz_s02, "object/tangible/wearables/goggles/goggles_kubaz_s02.iff")
