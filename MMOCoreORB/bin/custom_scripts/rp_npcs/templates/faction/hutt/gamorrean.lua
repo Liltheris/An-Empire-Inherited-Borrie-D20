ebe8aa0086c038daf95dfd244c7893a6aca00c8c
Whiskey <RobinTh11@googlemail.com>
2025-01-18 20:20:33 +0100

    oink

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/templates/faction/hutt/gamorrean.lua
@@ -0,0 +1,12 @@
+npc_template = {
+
+	name = "a Gamorrean Guard",
+	baseTemplates = {
+		"gamorrean", 10,
+	},
+	skillTemplates = {"common/soldier_strong"},
+	
+	equipmentTemplates = {},	
+	customizationTemplates = {},	
+	customVarOverrides = {}
+}
