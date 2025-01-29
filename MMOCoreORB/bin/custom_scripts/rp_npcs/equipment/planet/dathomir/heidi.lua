50450f4eda6df11dda134c151b621a6ecba2552c
Valerie <liltheris@gmail.com>
2025-01-29 22:32:16 +0000

    pushback

---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/planet/dathomir/heidi.lua
@@ -0,0 +1,10 @@
+equipment = {
+	{"object/tangible/wearables/armor/nightsister/armor_nightsister_bicep_r_s01.iff", },
+	{"object/tangible/wearables/shirt/nightsister_shirt_s03.iff", "/private/index_color_2", 3, "/private/index_color_1", 115, },
+	{"object/tangible/wearables/cape/cape_kama_s01.iff", "/private/index_color_2", 110, "/private/index_color_1", 31, },
+	{"object/tangible/wearables/cape/cape_kama_s01.iff", "/private/index_color_2", 110, "/private/index_color_1", 31, },
+	{"object/tangible/wearables/mask/mask_sith_s01.iff", "/private/index_color_2", 132, },
+	{"object/tangible/wearables/mask/mask_bandana_s01.iff", "/private/index_color_0", 90, },
+	{"object/tangible/wearables/pants/nightsister_pants_s02.iff", "/private/index_color_2", 19, "/private/index_color_1", 3, },
+	{"object/tangible/wearables/boots/nightsister_boots.iff", "/private/index_color_1", 3, },
+}
---
+++ MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/unique/whiskey/private_army_s01.lua
@@ -0,0 +1,14 @@
+equipment = {
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_bicep_l.iff", "/private/index_color_2", 42, "/private/index_color_1", 160, "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_bicep_r.iff", "/private/index_color_2", 42, "/private/index_color_1", 160, "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_bracer_l.iff", "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_bracer_r.iff", "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_bracer_l.iff", "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_bracer_r.iff", "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/padded/armor_padded_s01_chest_plate.iff", "/private/index_color_2", 160, "/private/index_color_1", 42, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_gloves.iff", "/private/index_color_3", 156, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_leggings.iff", "/private/index_color_2", 6, "/private/index_color_1", 6, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_leggings.iff", "/private/index_color_2", 6, "/private/index_color_1", 6, },
+	{"object/tangible/wearables/armor/roleplay/marine/armor_marine_boots.iff", "/private/index_color_3", 156, },
+	{"object/tangible/wearables/belt/belt_s07.iff", "/private/index_color_1", 31, },
+}
