554576b7d8141dcf737dc1b332f571387034092e
Valerie <liltheris@gmail.com>
2025-02-24 16:29:34 +0000

    Reverted chaos

--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/dev/bili/indy.lua
+++
@@ -1,7 +0,0 @@
-equipment = {
-	{"object/tangible/wearables/vest/vest_s03.iff", "/private/index_color_2", 48, "/private/index_color_1", 48, },
-	{"object/tangible/wearables/hat/hat_fedora__s01.iff", },
-	{"object/tangible/wearables/pants/pants_s01.iff", "/private/index_color_2", 253, "/private/index_color_1", 41, },
-	{"object/tangible/wearables/shoes/shoes_s01.iff", },
-	{"object/tangible/wearables/belt/belt_whip_s01.iff", },
-}
--- MMOCoreORB/bin/custom_scripts/rp_npcs/equipment/dev/bili/jorj.lua
+++
@@ -1,7 +0,0 @@
-equipment = {
-	{"object/tangible/wearables/shirt/shirt_s10.iff", "/private/index_color_2", 125, "/private/index_color_1", 132, },
-	{"object/tangible/wearables/goggles/glasses_s01.iff", },
-	{"object/tangible/wearables/pants/pants_s05.iff", "/private/index_color_2", 160, "/private/index_color_1", 202, },
-	{"object/tangible/wearables/shoes/shoes_s02.iff", "/private/index_color_2", 91, "/private/index_color_1", 91, },
-	{"object/tangible/wearables/belt/belt_s16.iff", "/private/index_color_1", 91, },
-}
