36e774dd871353ac346d96184e5ba31962b218d0
Valerie <liltheris@gmail.com>
2025-04-06 13:36:30 +0100

    Spout pushback

---
+++ MMOCoreORB/bin/custom_scripts/spout/planet/tatooine/mepd/swat_standing.lua
@@ -0,0 +1,2 @@
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -6.21598, -3.97235, -9.66146, 0, 610008454, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -6.32326, -3.97235, -7.80086, 177, 610008454, "object/mobile/rp_human_female.iff", "default", "default", "default") --Equip, Skill, Customization
