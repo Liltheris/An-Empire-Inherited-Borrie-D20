8d24c63ec468ce88d972f2c5a9a358634c0731cf
Valerie <liltheris@gmail.com>
2025-04-06 13:21:27 +0100

    Spout pushback

---
+++ MMOCoreORB/bin/custom_scripts/spout/planet/tatooine/mepd/officer_sitting.lua
@@ -0,0 +1,7 @@
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, 6.22988, 0.172025, -1.93499, 174, 610008455, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, 9.28508, 0.172025, -1.96492, 177, 610008455, "object/mobile/rp_human_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, 9.27013, 0.172025, -4.95448, 175, 610008455, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, 3.27155, 0.172025, -4.97202, 172, 610008455, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -8.77239, -0.156781, 0.152693, 0, 610008453, "object/mobile/rp_human_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -8.89544, -0.156781, 2.59386, 0, 610008453, "object/mobile/rp_nikto_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -8.94615, -0.156781, 4.88315, 0, 610008453, "object/mobile/rp_rodian_male.iff", "default", "default", "default") --Equip, Skill, Customization
---
+++ MMOCoreORB/bin/custom_scripts/spout/planet/tatooine/mepd/officer_standing.lua
@@ -0,0 +1,7 @@
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -4.54365, 0.307649, 6.91727, 73, 610008453, "object/mobile/rp_human_female.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -3.81011, 0.304384, 7.65657, 193, 610008453, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -3.38565, 0.304239, 6.83057, 280, 610008453, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -4.39888, 0.300589, -5.85485, 149, 610008453, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -3.72524, 0.299032, -6.55629, 313, 610008453, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, -9.69001, -0.156781, 0.158965, 0, 610008453, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, 7.28963, 0.489744, 3.69274, 89, 610008452, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
