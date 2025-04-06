61428688343fe0c88ff43dd878bcb69bc9f97cf6
Valerie <liltheris@gmail.com>
2025-04-06 13:42:37 +0100

    Spout pushback

---
+++ MMOCoreORB/bin/custom_scripts/spout/planet/tatooine/mepd/magistrate.lua
@@ -0,0 +1,1 @@
+spawnRoleplayMobile("rp_tatooine", "rp_base_npc", 1, 2.33481, 0.489743, 7.48859, 180, 610008452, "object/mobile/rp_human_male.iff", "default", "default", "default") --Equip, Skill, Customization
